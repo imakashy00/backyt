ea09ba06c343cabefaaec25fddd0aa7e01f1d699
Akash Yadav <imakashy00@gmail.com>
2025-03-04 11:36:52 +0530

    working 1

--- app/database/db.py
+++ app/database/db.py
@@ -5,7 +5,13 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 load_dotenv()
 
-DATABASE_URI = os.getenv('DATABASE_URI')
+# Get database URL with a fallback
+DATABASE_URI = os.getenv("DATABASE_URI")
+
+# Add validation to ensure DATABASE_URL is not None
+if not DATABASE_URI:
+    raise ValueError("DATABASE_URL environment variable is not set")
+
 
 engine  = create_engine(DATABASE_URI,echo= True,pool_pre_ping=True)
 SessionLocal = sessionmaker(bind = engine, autocommit = False, autoflush = False)
